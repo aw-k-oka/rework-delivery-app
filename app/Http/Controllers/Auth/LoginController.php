29b04e2f72319c17c7642ed9a0ee59ca890ee6c2
岡 響平 <k_oka@F240215.local>
2026-05-21 13:29:30 +0900

    ログアウトでエラーになる不具合を修正

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -57,6 +57,6 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login.index');
+        return redirect()->route('login');
     }
 }

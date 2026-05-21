624fd207ec97b99154e5f5d39d4a80199863e1c2
岡 響平 <k_oka@F240215.local>
2026-05-21 15:02:35 +0900

    不要なdiv削除

--- resources/js/components/Complete.jsx
+++ resources/js/components/Complete.jsx
@@ -1,7 +1,5 @@
 export default function Complete({ trackingNumber }) {
     return (
-        <div>
-            <p>配送番号：{ trackingNumber }にて承りました</p>
-        </div>
+        <p>配送番号：{ trackingNumber }にて承りました</p>
     );
 }
